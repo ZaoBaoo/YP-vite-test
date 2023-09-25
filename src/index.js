7ef66434f1840b205c485a4a01a83629a88ab092
Yurkin Daniil <zaobaoo@yandex.kz>
2023-09-25 19:46:13 +0600

    push from work

--- src/index.js
+++ src/index.js
@@ -1,3 +1,24 @@
 import {identity} from './utils/mydash/identity.js';
 
-console.log(identity(123))
+/*
+isEmpty(null); // => true
+isEmpty(true); // => true
+isEmpty(1); // => true
+isEmpty([1, 2, 3]); // => false
+isEmpty({ 'a': 1 }); // => false
+isEmpty('123'); // => false
+isEmpty(123); // => true
+isEmpty(''); // => true
+isEmpty(0); // => true
+isEmpty(undefined) // => true
+isEmpty(new Map([['1', 'str1'], [1, 'num1'], [true, 'bool1']])) // => false
+isEmpty(new Set(['value1', 'value2', 'value3'])) // => false
+*/
+
+function isEmpty(value) {
+    if(value === null) return true;
+    if(typeof value === 'string' && value.length) return false
+    return typeof value !== 'object';
+}
+
+console.log(isEmpty(undefined))
